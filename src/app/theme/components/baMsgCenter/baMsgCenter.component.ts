0b84d32f3248e62ffd425fd86d48d2da9c918685
SarojK <saroj.kaashyap275@gmail.com>
2017-05-01 12:54:54 +0530

    Small changes to web application

--- src/app/theme/components/baMsgCenter/baMsgCenter.component.ts
+++ src/app/theme/components/baMsgCenter/baMsgCenter.component.ts
@@ -11,11 +11,9 @@ import {BaMsgCenterService} from './baMsgCenter.service';
 export class BaMsgCenter {
 
   public notifications:Array<Object>;
-  public messages:Array<Object>;
 
   constructor(private _baMsgCenterService:BaMsgCenterService) {
     this.notifications = this._baMsgCenterService.getNotifications();
-    this.messages = this._baMsgCenterService.getMessages();
   }
 
 }
